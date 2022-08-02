360cfdb8ed48ef406c44b93a03cc9a13563d90b4
mohamed <mh95568@gmail.com>
2022-08-02 22:03:30 +0200

    second commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,11 @@ fun main(args: Array<String>) {
     val number1 = 20
     val number2 = 452
     println(number1 + number2)
-    
+    // second commit
+
+    val x : Float = 30.15f
+    val y : Float = 123.54f
+    println(x + y)
+
 
 }
